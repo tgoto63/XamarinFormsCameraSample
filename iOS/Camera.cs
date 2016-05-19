6160847c90b6d77afc79b4eb15be24b370688325
goto63 <tgoto63@gmail.com>
2016-05-19 09:23:00 +0700

    Implements Cameraprovider.iOS

--- iOS/Camera.cs
+++ iOS/Camera.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using Foundation;
+using UIKit;
+
 namespace XamarinFormsCameraSample.iOS
 {
-    public class Camera
+    public static class Camera
     {
-        public Camera()
+        static UIImagePickerController picker;
+        static Action<NSDictionary> _callback;
+
+        static void Init()
         {
+            if (picker != null)
+                return;
+
+            picker = new UIImagePickerController();
+            picker.Delegate = new CameraDelegate();
+        }
+
+        class CameraDelegate : UIImagePickerControllerDelegate
+        {
+            public override void FinishedPickingMedia(UIImagePickerController picker, NSDictionary info)
+            {
+                var cb = _callback;
+                _callback = null;
+
+                picker.DismissViewController(true, (Action)null);
+                cb(info);
+            }
+
+            public override void Canceled(UIImagePickerController picker)
+            {
+                var cb = _callback;
+                _callback = null;
+
+                picker.DismissViewController(true, (Action)null);
+                cb(null);
+            }
+        }
+
+        public static void TakePicture(UIViewController parent, Action<NSDictionary> callback)
+        {
+            Init();
+            picker.SourceType = UIImagePickerControllerSourceType.Camera;
+            _callback = callback;
+            parent.PresentViewController((UIViewController)picker, true, (Action)null);
+        }
+
+        public static void SelectPicture(UIViewController parent, Action<NSDictionary> callback)
+        {
+            Init();
+            picker.SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+            _callback = callback;
+            parent.PresentViewController((UIViewController)picker, true, (Action)null);
         }
     }
 }
--- iOS/CameraProvider.cs
+++ iOS/CameraProvider.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using XamarinFormsCameraSample.iOS;
+
+[assembly: Dependency(typeof(CameraProvider))]
 namespace XamarinFormsCameraSample.iOS
 {
-    public class CameraProvider
+    public class CameraProvider : ICameraProvider
     {
-        public CameraProvider()
+        public Task<CameraResult> TakePictureAsync()
         {
+            var tcs = new TaskCompletionSource<CameraResult>();
+
+            Camera.TakePicture(UIApplication.SharedApplication.KeyWindow.RootViewController, (imagePickerResult) =>
+            {
+
+                if (imagePickerResult == null)
+                {
+                    tcs.TrySetResult(null);
+                    return;
+                }
+
+                var photo = imagePickerResult.ValueForKey(new NSString("UIImagePickerControllerOriginalImage")) as UIImage;
+
+                // You can get photo meta data with using the following
+                // var meta = obj.ValueForKey(new NSString("UIImagePickerControllerMediaMetadata")) as NSDictionary;
+
+                var documentsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                string jpgFilename = Path.Combine(documentsDirectory, Guid.NewGuid() + ".jpg");
+                NSData imgData = photo.AsJPEG();
+                NSError err = null;
+
+                if (imgData.Save(jpgFilename, false, out err))
+                {
+                    CameraResult result = new CameraResult();
+                    result.Image = ImageSource.FromStream(imgData.AsStream);
+                    result.FileUri = jpgFilename;
+
+                    tcs.TrySetResult(result);
+                }
+                else
+                {
+                    tcs.TrySetException(new Exception(err.LocalizedDescription));
+                }
+            });
+
+            return tcs.Task;
         }
     }
 }
